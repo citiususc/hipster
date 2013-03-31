e6d6e2eba24b9db950bc59042cc2a8bf793e92e7
adrian_gonzalez <adrian.gonzalez@usc.es>
2013-03-31 17:59:20 +0200

    Changed license header in all files.

--- src/test/java/es/usc/citius/lab/hipster/maze/StringMazeTest.java
+++ src/test/java/es/usc/citius/lab/hipster/maze/StringMazeTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Centro de Investigación en Tecnoloxías da Información (CITIUS).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package es.usc.citius.lab.hipster.maze;
 
 import static org.junit.Assert.*;
@@ -10,45 +26,44 @@ import es.usc.citius.lab.hipster.util.maze.StringMaze;
 
 public class StringMazeTest {
 
-	@Test
-	public void test() {
-		String [] testMaze = {
-			      "XX@XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-			      "XX XXXXXXXXXXXXX     XXXXXXXXXXX",
-			      "XX    XXXXXXXXXX XXX XX     XXXX",
-			      "XXXXX  XXXXXX    XXX XX XXX XXXX",
-			      "XXX XX XXXXXX XX XXX XX  XX XXXX",
-			      "XXX     XXXXX XXXXXX XXXXXX XXXX",
-			      "XXXXXXX       XXXXXX        XXXX",
-			      "XXXXXXXXXX XXXXX XXXXXXXXXXXXXXX",
-			      "XXXXXXXXXX XX    XXXXX      XXXX",
-			      "XXXXXXXXXX    XXXXXXXX XXXX XXXX",
-			      "XXXXXXXXXXX XXXXXXXXXX XXXX XXXX",
-			      "XXXXXXXXXXX            XXXX XXXX",
-			      "XXXXXXXXXXXXXXXXXXXXXXXX XX XXXX",
-			      "XXXXXX              XXXX XX XXXX",
-			      "XXXXXX XXXXXXXXXXXX XX      XXXX",
-			      "XXXXXX XXO   XXXXXX XXXX XXXXXXX",
-			      "XXXXXX XXXXX   XXX            XX",
-			      "XXXXXX XXXXXXX XXXXXXXXXXX XXXXX",
-			      "XXXXXX XXXXXXX XXXXXXXXXXXXXXXXX",
-			      "XXXXXX            XXXXXXXXXXXXXX"};
-		StringMaze maze = new StringMaze(testMaze);
-		// Valid cells
-		assertTrue(maze.getMaze()[2][1]);
-		assertTrue(maze.getMaze()[2][0]);
-		assertTrue(maze.getMaze()[4][5]);
-		// Invalid cells
-		assertFalse(maze.getMaze()[0][0]);
-		assertFalse(maze.getMaze()[1][1]);
-		assertFalse(maze.getMaze()[12][5]);
-		// Initial loc
-		assertEquals(maze.getInitialLoc(), new Point(2,0));
-		assertEquals(maze.getGoalLoc(), new Point(9,15));
-		// Print valid moves from initial and goal
-		System.out.println(maze.validLocationsFrom(maze.getInitialLoc()));
-		System.out.println(maze.validLocationsFrom(maze.getGoalLoc()));
-		System.out.println(maze.validLocationsFrom(new Point(3,14)));
-	}
-
+    @Test
+    public void test() {
+        String[] testMaze = {
+            "XX@XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "XX XXXXXXXXXXXXX     XXXXXXXXXXX",
+            "XX    XXXXXXXXXX XXX XX     XXXX",
+            "XXXXX  XXXXXX    XXX XX XXX XXXX",
+            "XXX XX XXXXXX XX XXX XX  XX XXXX",
+            "XXX     XXXXX XXXXXX XXXXXX XXXX",
+            "XXXXXXX       XXXXXX        XXXX",
+            "XXXXXXXXXX XXXXX XXXXXXXXXXXXXXX",
+            "XXXXXXXXXX XX    XXXXX      XXXX",
+            "XXXXXXXXXX    XXXXXXXX XXXX XXXX",
+            "XXXXXXXXXXX XXXXXXXXXX XXXX XXXX",
+            "XXXXXXXXXXX            XXXX XXXX",
+            "XXXXXXXXXXXXXXXXXXXXXXXX XX XXXX",
+            "XXXXXX              XXXX XX XXXX",
+            "XXXXXX XXXXXXXXXXXX XX      XXXX",
+            "XXXXXX XXO   XXXXXX XXXX XXXXXXX",
+            "XXXXXX XXXXX   XXX            XX",
+            "XXXXXX XXXXXXX XXXXXXXXXXX XXXXX",
+            "XXXXXX XXXXXXX XXXXXXXXXXXXXXXXX",
+            "XXXXXX            XXXXXXXXXXXXXX"};
+        StringMaze maze = new StringMaze(testMaze);
+        // Valid cells
+        assertTrue(maze.getMaze()[2][1]);
+        assertTrue(maze.getMaze()[2][0]);
+        assertTrue(maze.getMaze()[4][5]);
+        // Invalid cells
+        assertFalse(maze.getMaze()[0][0]);
+        assertFalse(maze.getMaze()[1][1]);
+        assertFalse(maze.getMaze()[12][5]);
+        // Initial loc
+        assertEquals(maze.getInitialLoc(), new Point(2, 0));
+        assertEquals(maze.getGoalLoc(), new Point(9, 15));
+        // Print valid moves from initial and goal
+        System.out.println(maze.validLocationsFrom(maze.getInitialLoc()));
+        System.out.println(maze.validLocationsFrom(maze.getGoalLoc()));
+        System.out.println(maze.validLocationsFrom(new Point(3, 14)));
+    }
 }
