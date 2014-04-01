2517de987fb5430d4f8c2f080232f60dde3c88ab
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2014-04-01 13:21:46 +0200

    BreadthFirstSearch fixed. Now it creates a new iterator each time the method iterator() is called.

--- hipster-core/src/main/java/es/usc/citius/hipster/algorithm/BreadthFirstSearch.java
+++ hipster-core/src/main/java/es/usc/citius/hipster/algorithm/BreadthFirstSearch.java
@@ -24,46 +24,57 @@ import es.usc.citius.hipster.model.function.NodeFactory;
 import java.util.*;
 
 public class BreadthFirstSearch<A,S,N extends Node<A,S,N>> extends Algorithm<A,S,N> {
-    private Queue<N> queue = new LinkedList<N>();
-    private NodeFactory<A,S,N> nodeFactory;
-    private Map<S, N> visited = new HashMap<S, N>();
-    private ActionStateTransitionFunction<A,S> tf;
+    private final S initialState;
+    private final ActionStateTransitionFunction<A,S> tf;
+    private final NodeFactory<A,S,N> nodeFactory;
 
-    public BreadthFirstSearch(S initialState, ActionStateTransitionFunction<A,S> transitionFunction,  NodeFactory<A,S,N> nodeFactory) {
-        N initialNode = nodeFactory.makeNode(null, new ActionState<A, S>(null, initialState));
-        this.nodeFactory = nodeFactory;
+    public BreadthFirstSearch(S initialState, ActionStateTransitionFunction<A,S> transitionFunction, NodeFactory<A,S,N> nodeFactory) {
+        this.initialState = initialState;
         this.tf = transitionFunction;
-        this.visited.put(initialState, initialNode);
-        queue.add(initialNode);
+        this.nodeFactory = nodeFactory;
+    }
+
+    /**
+     * Implements all the BFS search logic as an interator
+     */
+    private class BFSIter implements Iterator<N>{
+        private Queue<N> queue = new LinkedList<N>();
+        private Map<S, N> visited = new HashMap<S, N>();
+
+        public BFSIter(){
+            N initialNode = nodeFactory.makeNode(null, new ActionState<A, S>(null, initialState));
+            visited.put(initialState, initialNode);
+            queue.add(initialNode);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !queue.isEmpty();
+        }
+
+        @Override
+        public N next() {
+            // Take next node
+            N current = queue.poll();
+            S currentState = current.state();
+            for(ActionState<A,S> transition : tf.transitionsFrom(currentState)){
+                if (!visited.containsKey(transition.getState())){
+                    N successorNode = nodeFactory.makeNode(current, transition);
+                    visited.put(transition.getState(), successorNode);
+                    queue.add(successorNode);
+                }
+            }
+            return current;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 
     @Override
     public Iterator<N> iterator() {
-        return new Iterator<N>() {
-            @Override
-            public boolean hasNext() {
-                return !queue.isEmpty();
-            }
-
-            @Override
-            public N next() {
-                // Take next node
-                N current = queue.poll();
-                S currentState = current.state();
-                for(ActionState<A,S> transition : tf.transitionsFrom(currentState)){
-                    if (!visited.containsKey(transition.getState())){
-                        N successorNode = nodeFactory.makeNode(current, transition);
-                        visited.put(transition.getState(), successorNode);
-                        queue.add(successorNode);
-                    }
-                }
-                return current;
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-        };
+        return new BFSIter();
     }
 }
