3e978fbae057ce57c71e54ee506d5ec67044a37d
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2014-04-02 09:51:29 +0200

    Implementation of the heuristic node

---
+++ hipster-core/src/main/java/es/usc/citius/hipster/model/impl/HeuristicNodeImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 CITIUS <http://citius.usc.es>, University of Santiago de Compostela.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package es.usc.citius.hipster.model.impl;
+
+
+import es.usc.citius.hipster.model.AbstractNode;
+import es.usc.citius.hipster.model.HeuristicNode;
+
+public class HeuristicNodeImpl<A,S,C extends Comparable<C>>
+        extends AbstractNode<A,S,HeuristicNodeImpl<A,S,C>>
+        implements HeuristicNode<A,S,C, HeuristicNodeImpl<A,S,C>> {
+
+
+    private C cost;
+    private C estimation;
+    private C score;
+
+    public HeuristicNodeImpl(HeuristicNodeImpl<A, S, C> previousNode, S state, A action, C cost, C estimation, C score) {
+        super(previousNode, state, action);
+        this.cost = cost;
+        this.estimation = estimation;
+        this.score = score;
+    }
+
+    @Override
+    public C getScore() {
+        return score;
+    }
+
+    @Override
+    public C getEstimation() {
+        return estimation;
+    }
+
+    @Override
+    public C getCost() {
+        return cost;
+    }
+}
