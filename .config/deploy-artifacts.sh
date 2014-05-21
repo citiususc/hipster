705a8eddb315efc70463209ad588661b6b8563a9
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2014-05-21 18:16:07 +0200

    added configuration for auto deployment with travis-ci

---
+++ .config/deploy-artifacts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Auto-deploying Hipster artifacts..."
+echo "Current branch: $TRAVIS_BRANCH"
+
+if [ "$TRAVIS_REPO_SLUG" == "citiususc/hipster" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo "Running mvn deploy..."
+  mvn deploy --settings .config/maven-settings.xml -DskipTests=true
+else
+  echo "Skipping deployment for this build..."
+fi
+
+echo "Deployment script finished." 
