0dd0546305d050b6e123674be18029cc0a2afac2
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2014-11-08 19:36:52 +0100

    CostFunction Javadoc updated [skip ci]

--- hipster-core/src/main/java/es/usc/citius/hipster/model/function/CostFunction.java
+++ hipster-core/src/main/java/es/usc/citius/hipster/model/function/CostFunction.java
@@ -20,7 +20,7 @@ package es.usc.citius.hipster.model.function;
 import es.usc.citius.hipster.model.Transition;
 
 /**
- * The cost function evaluates a ---
+ * The cost function evaluates a transition
  * between states. A search algorithm will find the solution that minimizes the
  * total cost of the function defined here.
  * 
