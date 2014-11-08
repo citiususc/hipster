aa9e65c3ba381091d3477e3e4dbc8affd7f7d752
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2014-11-08 19:51:54 +0100

    SearchProblem Javadoc updated [skip ci]

--- hipster-core/src/main/java/es/usc/citius/hipster/model/problem/SearchProblem.java
+++ hipster-core/src/main/java/es/usc/citius/hipster/model/problem/SearchProblem.java
@@ -4,6 +4,10 @@ import es.usc.citius.hipster.model.Node;
 import es.usc.citius.hipster.model.function.NodeExpander;
 
 /**
+ * Defines a search problems in terms of a initial node to start with and the node expander
+ * function that generates new successor nodes. A SearchProblem can be fully defined using
+ * the {@link es.usc.citius.hipster.model.problem.ProblemBuilder} class assistant builder.
+ *
  * @author Pablo Rodríguez Mier <<a href="mailto:pablo.rodriguez.mier@usc.es">pablo.rodriguez.mier@usc.es</a>>
  */
 public class SearchProblem<A,S,N extends Node<A,S,N>> {
